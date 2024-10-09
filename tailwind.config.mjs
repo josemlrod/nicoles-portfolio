a387bbca3f3fa3e7002dc217ebfecf2beb6ad0a7
nicole <nicolerodriguezcab@gmail.com>
2024-10-08 21:34:45 -0400

    added styling sitewide

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-    fontFamily: {
-      sans: ['"Poppins", sans-serif'],
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		screens: {
+			lg: "1280px",
+			md: "1024px",
+			sm: "768px",
+			xs: "640px",
+			xxs: "378px",
+		},
+		extend: {},
+		fontFamily: {
+			sans: ['"Poppins", sans-serif'],
+		},
+	},
+	plugins: [],
 };
